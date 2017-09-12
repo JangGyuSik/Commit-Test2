9023876af901323d182589cd5134cdcecf46dd78
D7702_10 <d7702_10@d7702-10ui-imac.local>
2017-09-12 10:45:15 +0900

    add branch

--- Jang_Git_test2/ViewController.swift
+++ Jang_Git_test2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("abc")
         print("commit Test")
+        print("branch 추가")
     }
 
     override func didReceiveMemoryWarning() {
